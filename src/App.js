170c6dc7f24259d64f6e194598b4df4af4b6acaa
Nguyen Vy <nttvy27@gmail.com>
2023-04-29 11:24:49 +0700

    Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import Banner from './components/Home/Banner';
 
 function App() {
     return (
         <div className="font-bodyFont">
             <Header />
+            <Banner />
         </div>
     );
 }
